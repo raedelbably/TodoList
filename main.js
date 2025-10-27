773e911d991dde9e8a465c7325764b8b3c3dd8cc
raedelbably <140548995+raedelbably@users.noreply.github.com>
2025-10-27 10:47:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+let noteInput = document.getElementById("noteInput");
+let addNote = document.getElementById("addNote");
+let notesList = document.getElementById("notesList");
+let dark = document.getElementById("dark");
+let light = document.getElementById("light");
+let container = document.getElementById("container");
+let notes = JSON.parse(localStorage.getItem("notes")) || []; // localstorge the notes
+// add new note in the notesList
+function addnewnotes() {
+  let note = noteInput.value;
+  if (noteInput.value !== "") {
+    notesList.innerHTML += `<li> ${note} <button  onclick="removenote(this)">remove</button> <button class ='update'>update</button> </li>`;
+    noteInput.value = "";
+    notes.push(note);
+    localStorage.setItem("notes", JSON.stringify(notes));
+  } else {
+    alert("please enter notes");
+  }
+}
+
+
+// remove one the note
+function removenote(button) {
+  let listnote = button.parentElement.firstChild.textContent.trim();
+  notes = notes.filter((note) => note.trim() !== listnote);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  button.parentElement.remove();
+}
+// update the note
+function updatenote(button) {
+  let listnote = button.parentElement.firstChild.textContent.trim();
+  noteInput.value = listnote;
+  notes = notes.filter((note) => note.trim() !== listnote);
+  if (notes.includes(noteInput.value)) {
+    alert("this note is already exists");
+  }
+  localStorage.setItem("notes", JSON.stringify(notes));
+  button.parentElement.remove();
+}
+
+// add background light and dark
+let containers = document.getElementById("container");
+let notesLists = document.getElementById("notesList");
+
+function notedark() {
+  containers.style.background = "black";
+  containers.style.color = "white";
+  notesLists.style.color = "#333";
+  localStorage.setItem("theme", "dark");
+}
+
+function notelight() {
+  containers.style.background = "#fff";
+  containers.style.color = "#333";
+  localStorage.setItem("theme", "light");
+}
+
+// عند تحميل الصفحة، نتحقق من الوضع المخزن
+window.onload = function() {
+    notes.forEach((note) => {
+    notesList.innerHTML += `<li> ${note} <button onclick="removenote(this)">remove</button> <button class = 'update' onclick="updatenote(this)"> update </button>  </li>`;
+  });
+  const theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    notedark();
+  } else {
+    notelight();
+  }
+};
+
